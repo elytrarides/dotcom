45302eeddcc40be52c92dcad18b41b4d3079cc44
Max Koon <max@koon.dev>
2023-06-23 11:44:14 -0400

    feat: port from root

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div id="landing">
+            <div>
+                <img 
+                    style={{
+
+                    }}
+                src={`${window.location.origin}/landing.png`} />
+                <div id="text">
+                    <h1>Coming January 2024...</h1>
+                    <a href="https://forms.gle/FpnCU8gzT6UgDnJa6">Join Waitlist</a>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
